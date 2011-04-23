85023e88263d2ec4956b2156e31ef0a7d252ba5c
mambro007@gmail.com <mambro007@gmail.com@7351564b-7ef7-4e93-ed4a-9a095f9b2759>
2011-04-23 14:24:21 +0000

    Creazione classe Server per l'implementazione

---
+++ CODICEcamdinzzle/src/Server.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+
+/**
+ * @author Andrea
+ *
+ */
+public class Server {
+
+	/**
+	 * 
+	 */
+	public Server() {
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
