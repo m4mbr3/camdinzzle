80d029a8adb1da6fdc410bad9af98fec5c36a0fb
mambro007@gmail.com <mambro007@gmail.com@7351564b-7ef7-4e93-ed4a-9a095f9b2759>
2011-05-07 09:57:55 +0000

    Update login area

--- CODICEcamdinzzle/src/Server/Server.java
+++ CODICEcamdinzzle/src/Server/Server.java
@@ -11,17 +11,26 @@ public class Server {
 	/**
 	 * 
 	 */
+	private ServerSocket deal_server;
+	private ServerSocket deal_login;
+	private Socket deal_socket;
+	private Game currentSession;
+	private int port_login;
 	public Server() 
 	{
 		// TODO Auto-generated constructor stub
-		
+		//Definition of a default port login
+		port_login = 4567;
+		//Definition of new Server login for passing it to startLogin 
+		deal_login = new ServerSocket(port_login);
 	}
 	public void controlAction()
 	{
 		
 	}
-	public void login()
+	public void Startlogin()
 	{
+		/*The connection for login must be at this port : 4567	*/
 		
 	}
 	public void newUser()
